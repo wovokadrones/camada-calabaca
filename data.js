99dc35550f734b93ad20caaaaf750eb6155baa18
geoscriber <100984391+geoscriber@users.noreply.github.com>
2024-02-07 22:22:13 +0800

    initial commit

---
+++ data.js
@@ -0,0 +1,418 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-calabaca-1",
+      "name": "Calabaca 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 2.669578535040766,
+        "pitch": 0.2748963714900352,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.608320489723951,
+          "pitch": 0.6261180143067655,
+          "rotation": 0,
+          "target": "2-calabaca-3"
+        },
+        {
+          "yaw": 2.3214782971778707,
+          "pitch": 0.14064131555065984,
+          "rotation": 0,
+          "target": "1-calabaca-2"
+        },
+        {
+          "yaw": 2.8331903655060477,
+          "pitch": -0.004152818388849511,
+          "rotation": 0,
+          "target": "3-calabaca-4"
+        },
+        {
+          "yaw": -3.105589376366865,
+          "pitch": 0.4189933322836268,
+          "rotation": 0,
+          "target": "5-calabaca-6"
+        },
+        {
+          "yaw": -3.0372666650493105,
+          "pitch": 0.0011775370774032012,
+          "rotation": 0,
+          "target": "4-calabaca-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-calabaca-2",
+      "name": "Calabaca 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 1.7700899488357713,
+        "pitch": 1.5707963267948966,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.247712187222917,
+          "pitch": 0.3996925758514127,
+          "rotation": 0,
+          "target": "0-calabaca-1"
+        },
+        {
+          "yaw": -2.9422990632553887,
+          "pitch": 1.418483330794535,
+          "rotation": 0,
+          "target": "2-calabaca-3"
+        },
+        {
+          "yaw": 0.2974360263208293,
+          "pitch": 0.530679232495455,
+          "rotation": 0,
+          "target": "5-calabaca-6"
+        },
+        {
+          "yaw": -0.5257093655732028,
+          "pitch": 0.023200260859507438,
+          "rotation": 0,
+          "target": "3-calabaca-4"
+        },
+        {
+          "yaw": 0.05422897818661454,
+          "pitch": -0.010010807967049118,
+          "rotation": 0,
+          "target": "4-calabaca-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-calabaca-3",
+      "name": "Calabaca 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 0.38507509810256657,
+        "pitch": 0.5152582943275537,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1666987809607097,
+          "pitch": -0.6023247677984447,
+          "rotation": 0,
+          "target": "1-calabaca-2"
+        },
+        {
+          "yaw": -0.3354750876266639,
+          "pitch": -0.26860279951379695,
+          "rotation": 0,
+          "target": "3-calabaca-4"
+        },
+        {
+          "yaw": 0.20707359942847603,
+          "pitch": -0.14357573011169933,
+          "rotation": 0,
+          "target": "4-calabaca-5"
+        },
+        {
+          "yaw": 1.8879430418610577,
+          "pitch": -0.41538723459174776,
+          "rotation": 0,
+          "target": "0-calabaca-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-calabaca-4",
+      "name": "Calabaca 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 0.6427632038133755,
+        "pitch": 0.3403187066328428,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5984916534774829,
+          "pitch": 0.45468738545889487,
+          "rotation": 0,
+          "target": "5-calabaca-6"
+        },
+        {
+          "yaw": 1.3045905223651655,
+          "pitch": 0.3199200284043098,
+          "rotation": 0,
+          "target": "2-calabaca-3"
+        },
+        {
+          "yaw": 1.373022242110963,
+          "pitch": -0.09187192726869142,
+          "rotation": 0,
+          "target": "1-calabaca-2"
+        },
+        {
+          "yaw": 1.0105685837379141,
+          "pitch": 0.019511488458455517,
+          "rotation": 0,
+          "target": "0-calabaca-1"
+        },
+        {
+          "yaw": -1.0104498019462014,
+          "pitch": 0.032938735217026505,
+          "rotation": 0,
+          "target": "4-calabaca-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-calabaca-5",
+      "name": "Calabaca 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": -0.042146667944193084,
+        "pitch": 0.5757274138374093,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7108054453209967,
+          "pitch": 0.16509898800168976,
+          "rotation": 0,
+          "target": "3-calabaca-4"
+        },
+        {
+          "yaw": -3.0624140502678063,
+          "pitch": 0.04857725353322451,
+          "rotation": 0,
+          "target": "1-calabaca-2"
+        },
+        {
+          "yaw": 2.9637954788991685,
+          "pitch": -0.007107167817061111,
+          "rotation": 0,
+          "target": "0-calabaca-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-calabaca-6",
+      "name": "Calabaca 6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": -2.918168805909641,
+        "pitch": 0.5526010409811466,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.403232430493471,
+          "pitch": -0.45767470135990784,
+          "rotation": 0,
+          "target": "3-calabaca-4"
+        },
+        {
+          "yaw": -2.817118965080436,
+          "pitch": -0.32039341048822934,
+          "rotation": 0,
+          "target": "4-calabaca-5"
+        },
+        {
+          "yaw": 0.06151861599018815,
+          "pitch": -0.24150656959301742,
+          "rotation": 0,
+          "target": "0-calabaca-1"
+        },
+        {
+          "yaw": 0.504970226334283,
+          "pitch": 0.09642230227670012,
+          "rotation": 0,
+          "target": "2-calabaca-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-camada-office",
+      "name": "CAMADA Office",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": -0.04848136811095216,
+        "pitch": 0.6318495872045897,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "CAMADA-Calabaca",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
